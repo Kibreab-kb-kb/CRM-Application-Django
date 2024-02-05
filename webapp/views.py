b595d99b058a2a629eb34439aa358fd17cba08fe
kibreab <kibreabteshome1993@gmail.com>
2024-02-05 22:11:49 +0300

    some update

--- webapp/views.py
+++ webapp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 def home(request):
     # return HttpResponse("Hello, Django!")
   return render(request,'webapps/index.html')
 
+
+
+ 
 
 
 
