4bc20d1767ab470e2c9a91dd7be8b019306001dd
kibreab <kibreabteshome1993@gmail.com>
2024-02-08 22:43:54 +0300

    creating registration view

--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms  import CreateUserForm,LoginForm
+
+
+
+#Homepage
 
 def home(request):
     # return HttpResponse("Hello, Django!")
   return render(request,'webapps/index.html')
+
+
+#register
+
+def register(request):
+    form = CreateUserForm()
+    if request.method=='POST':
+       form=CreateUserForm(request.POST)
+
+       if form.is_valid():
+          form.save()
+          # return redirect('')
+    
+
+    context={'form':form}
+       
+
+
+   
+ 
+
+
+
+
 
 
 
