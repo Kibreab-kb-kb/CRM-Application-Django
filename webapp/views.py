9659f43e3c1e25329d1394d340975d0d6d226c7d
kibreab <kibreabteshome1993@gmail.com>
2024-02-09 17:16:18 +0300

    working on login view

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,redirect
 from .forms  import CreateUserForm,LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
 
 
 
@@ -24,6 +28,33 @@ def register(request):
 
     context={'form':form}
     return render(request,'webapps/register.html',context=context)
+
+
+
+
+
+#login
+
+def login(request):
+   form=LoginForm()
+   if request.method=='POST':
+      form=LoginForm(request,data=request.POST)
+
+      if form.is_valid():
+         username=form.cleaned_data['username']
+         password=form.cleaned_data['password']
+
+         user=authenticate(request,username=username,password=password)
+      
+         if user is not None:
+          auth.login(request,user)
+          # redirect('')
+
+   context={'form':form}
+
+   return render(request,'webapps/login.html', context=context)
+
+
 
 
 
